65a5dc7e0687c529be9dd05f416b378617f59a1b
Zahidul Islam <31271800+zinayon@users.noreply.github.com>
2017-10-04 20:59:54 +0600

    Update string_times.py

--- Warmup-2/string_times.py
+++ Warmup-2/string_times.py
@@ -1,1 +1,16 @@
-a#
+# Given
+
+def string_times(str, n):
+  result = ""
+  for i in range(n):  # range(n) is [0, 1, 2, .... n-1]
+    result = result + str  # could use += here
+  return result
+
+#Solve 01
+
+def string_times(str, n):
+  return str * n
+
+#Solve 02
+
+
