162ffafd5efb7eea29ab84c44203b7c32b1267f1
Zahidul Islam <31271800+zinayon@users.noreply.github.com>
2017-10-04 20:35:42 +0600

    Update missing_char.py

--- Warmup-1/missing_char.py
+++ Warmup-1/missing_char.py
@@ -1,2 +1,17 @@
 #Given Solution
 def missing_char(str, n):
+  front = str[:n]   # up to but not including n
+  back = str[n+1:]  # n+1 through end of string
+  return front + back
+
+# Solution #1
+
+def missing_char(str, n):
+  return str[0:n] + [n+1:]
+
+#Solution #2
+
+def missing_char(str, n):
+  return str[0:n] + [n+1:]
+
+#
