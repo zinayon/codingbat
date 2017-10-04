ccf6a23c0e6a4ffc70e800ea9854a6a4e415e42c
Zahidul Islam <31271800+zinayon@users.noreply.github.com>
2017-10-04 20:19:54 +0600

    Update not_string.py

--- Warmup-1/not_string.py
+++ Warmup-1/not_string.py
@@ -1,5 +1,7 @@
 def not_string(str):
-  if len(str) >= 3 and str [:3] == "not":
+  if len(str) >= 3 and str [:3] == "not":     # len(str) = lenth of the string  and [:3] count of string from the begining
     return str
   else:
-    return "not "+str
+    return "not "+str  
+  
+  #####
