a900958e51117d330b746f08a86d220854671da8
Zahidul Islam <31271800+zinayon@users.noreply.github.com>
2017-10-04 20:51:22 +0600

    Update front3.py

--- Warmup-1/front3.py
+++ Warmup-1/front3.py
@@ -1,1 +1,33 @@
+#Given
 
+def front3(str):
+  # Figure the end of the front
+  front_end = 3
+  if len(str) < front_end:
+    front_end = len(str)
+  front = str[:front_end]
+  return front + front + front 
+  
+  # Could omit the if logic, and write simply front = str[:3]
+  # since the slice is silent about out-of-bounds conditions.
+  
+ #solution  01
+
+def front3(str):
+  if len(str) <= 3:
+    return str*3
+  front = str[:3]
+  
+  return front*3
+
+
+#Solution 02
+def front3(str):
+  if len(str) < 3:
+    return str+str+str
+  else:
+    return str[:3] + str[:3] + str[:3]
+  
+  #Solution 03
+  
+  
